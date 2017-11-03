34620616c98afef0bac44bd2dac05bcf7e4431e4
RayaIlieva <rajcheto@gmail.com>
2017-11-03 15:15:09 +0200

    added examples of nested dictionaries

---
+++ ExamsNestedDictionaries/p02SoftUnikaraoke/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02SoftUnikaraoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = new Dictionary<string,List<string>>();
+
+            var participants = Console.ReadLine().Split(',').Select(p => p.Trim()).ToList();
+            var songs = Console.ReadLine().Split(',').Select(s => s.Trim()).ToList();
+
+            var input = Console.ReadLine();
+
+
+            while (!input.Equals("dawn"))
+            {
+                var performance = input.Split(',').Select(p => p.Trim()).ToList();
+
+                var participant = performance[0];
+                var song = performance[1];
+                var award = performance[2];
+
+                if (participants.Contains(participant) && song.Contains(song))
+                {
+                    if (!results.ContainsKey(participant))
+                    {
+                        results[participant] = new List<string>();
+                    }
+
+                    var awards = results[participant];
+
+                    if (!awards.Contains(award))
+                    {
+                        awards.Add(award);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            if (results.Any())
+            {
+                foreach (var keyValuePair in results.OrderByDescending(p => p.Value.Count).ThenBy(n => n.Key))
+                {
+                    if (keyValuePair.Key.Any())
+                    {
+                        var participant = keyValuePair.Key;
+                        var awards = keyValuePair.Value;
+                        Console.WriteLine($"{participant}: {awards.Count} awards");
+                        foreach (var award in awards.OrderBy(n => n))
+                        {
+                            Console.WriteLine($"--{award}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No awards");
+            }
+
+        }
+    }
+}
---
+++ ExamsNestedDictionaries/p03EnduranceRally/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p03EnduranceRally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drivers = Console.ReadLine().Split(' ').ToList();
+            var zones = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var checkpoints = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int startingFuel = 0;
+            bool isFinished = true;
+
+            foreach (var driver in drivers)
+            {
+                startingFuel = (char)driver[0];
+
+                for (int i = 0; i < zones.Count; i++)
+                {
+                    
+                        if(checkpoints.Contains(i))
+                        {
+                            startingFuel += zones[i];
+                        }
+                        else
+                        {
+                            startingFuel -= zones[i];
+                        }
+
+                        if(startingFuel <= 0)
+                        {
+                            isFinished = false;
+                            Console.WriteLine($"{driver} - reached {i}");
+                            break;
+                        }
+                    
+                }
+
+                if (isFinished)
+                {
+                    Console.WriteLine($"{driver} - fuel left {startingFuel:F2}");
+                }
+
+
+            }
+        }
+    }
+}
