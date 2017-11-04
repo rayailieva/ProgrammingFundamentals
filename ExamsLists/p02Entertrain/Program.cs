09ce199d0e6164435203550a5cd532bf8dcc4c0c
RayaIlieva <rajcheto@gmail.com>
2017-11-04 15:24:40 +0200

    Lists problems from exams added

---
+++ ExamsLists/p02Entertrain/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02Entertrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            int locoPower = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine();
+
+            while (input != "All ofboard!")
+            {
+                    var wagons = (int.Parse(input));
+
+                    numbers.Add(wagons);
+
+                    int totalWeight = numbers.Sum();
+
+                    if(totalWeight > locoPower)
+                    {
+                        int average = totalWeight / numbers.Count;
+                        int closest = numbers.Aggregate((x, y) => Math.Abs(x - average) < Math.Abs(y - average) ? x : y);
+
+                        numbers.Remove(closest);
+                        numbers.Add(locoPower);
+                        numbers.Reverse();
+                    }
+
+                
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.Write(String.Join(" ", item));
+            }
+        }
+    }
+}
---
+++ ExamsLists/p02Icarus/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02Icarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] thePlane = Console.ReadLine()
+            .Split().Select(int.Parse).ToArray();
+
+            int index = int.Parse(Console.ReadLine());
+
+            int damage = 1;
+
+            string command = Console.ReadLine();
+            while (command != "Supernova")
+            {
+                string[] myArr = command.Split();
+                string direction = myArr[0];
+                int steps = int.Parse(myArr[1]);
+
+                switch (direction)
+                {
+                    case "left":
+
+                        for (int i = 0; i < steps; i++)
+                        {
+                            index--;
+
+                            if (index < 0)
+                            {
+                                index = thePlane.Length - 1;
+                                damage++;
+                            }
+
+                            thePlane[index] -= damage;
+                        }
+
+                        break;
+
+                    case "right":
+
+                        for (int i = 0; i < steps; i++)
+                        {
+                            index++;
+
+                            if (index > thePlane.Length - 1)
+                            {
+                                index = 0;
+                                damage++;
+                            }
+
+                            thePlane[index] -= damage;
+                        }
+
+                        break;
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", thePlane));
+        }
+    }
+}
